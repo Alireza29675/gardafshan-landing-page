9925900d49a5e8663b255ef36edb50c0c46b6a33
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-08-25 22:50:15 +0430

    Area has been made

--- src/Box.js
+++ src/Box.js
@@ -7,6 +7,9 @@ class Box {
     fixSizes () {
         this.openBoxImage.style.width = (this.closeBoxImage.offsetWidth * (464 / 300)) + 'px'
         this.openBoxImage.style.marginLeft = (this.closeBoxImage.offsetWidth / -200) + 'px'
+        const areaDistFromTop = (this.closeBoxImage.offsetWidth * (11 / 28))
+        $('.game > .area').style.top = areaDistFromTop + 'px'
+        $('.game > .area').style.height = (window.innerHeight - areaDistFromTop) + 'px'
     }
     open () {
         this.fixSizes()
