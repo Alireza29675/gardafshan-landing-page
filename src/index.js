052117bf63687ff0e51dd7b7e4e412961b1967f3
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-08-27 21:10:37 +0430

    Paper items can be checked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ImageLoader from './ImageLoader'
 import Box from './Box'
 import Vase from './Vase'
+import Paper from './Paper'
 
 window.$ = (query) => document.querySelector(query);
 window.$$ = (query) => document.querySelectorAll(query);
@@ -13,13 +14,21 @@ class Game {
         this.imagesLoader = new ImageLoader('./assets/images', this.ready.bind(this));
         this.box = new Box('.game .box');
         this.vase = new Vase('.game .vase')
+        this.paper = new Paper('.game .paper', this)
     }
     ready () {
         after(1000, this.unbox.bind(this))
     }
     unbox () {
         this.box.open();
-        this.vase.comeOut();
+        after(200, () => {
+            this.vase.comeOut();
+            after(100, () => {this.paper.comeOut();})
+            after(3000, () => {this.paper.next();})
+        })
+    }
+    onDone () {
+        console.log('Done')
     }
 }
 
