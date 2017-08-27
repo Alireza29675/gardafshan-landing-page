c3b48d5c471f53f65f65f0bdd6b4110bae841cb7
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-08-27 19:53:21 +0430

    Paper has been made

--- src/Box.js
+++ src/Box.js
@@ -1,22 +1,22 @@
 class Box {
     constructor (query) {
-        this.container = $(query)
-        this.openBoxImage = $(query + ' .open')
+        this.container = $(query);
+        this.openBoxImage = $(query + ' .open');
         this.closeBoxImage = $(query + ' .close')
     }
     fixSizes () {
-        this.openBoxImage.style.width = (this.closeBoxImage.offsetWidth * (464 / 300)) + 'px'
-        this.openBoxImage.style.marginLeft = (this.closeBoxImage.offsetWidth / -200) + 'px'
-        const areaDistFromTop = (this.closeBoxImage.offsetWidth * (11 / 28))
-        $('.game > .area').style.top = areaDistFromTop + 'px'
+        this.openBoxImage.style.width = (this.closeBoxImage.offsetWidth * (464 / 300)) + 'px';
+        this.openBoxImage.style.marginLeft = (this.closeBoxImage.offsetWidth / -200) + 'px';
+        const areaDistFromTop = (this.closeBoxImage.offsetWidth * (11 / 28));
+        $('.game > .area').style.top = areaDistFromTop + 'px';
         $('.game > .area').style.height = (window.innerHeight - areaDistFromTop) + 'px'
     }
     open () {
-        this.fixSizes()
+        this.fixSizes();
         this.container.classList.add('open')
     }
     close () {
-        this.fixSizes()
+        this.fixSizes();
         this.container.classList.remove('open')
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import ImageLoader from './ImageLoader'
 import Box from './Box'
 import Vase from './Vase'
 
-window.$ = (query) => document.querySelector(query)
-window.$$ = (query) => document.querySelectorAll(query)
+window.$ = (query) => document.querySelector(query);
+window.$$ = (query) => document.querySelectorAll(query);
 
-class App {
+window.after = (time, doWhat) => setTimeout(doWhat, time);
+window.every = (time, doWhat) => setInterval(doWhat, time);
+
+class Game {
     constructor () {
-        this.imagesLoader = new ImageLoader('./assets/images', this.ready.bind(this))
-        this.box = new Box('.game .box')
-        this.box.open()
+        this.imagesLoader = new ImageLoader('./assets/images', this.ready.bind(this));
+        this.box = new Box('.game .box');
         this.vase = new Vase('.game .vase')
     }
     ready () {
-        
+        after(1000, this.unbox.bind(this))
+    }
+    unbox () {
+        this.box.open();
+        this.vase.comeOut();
     }
 }
 
-const app = new App
+window.game = new Game();
